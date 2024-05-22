ce570ddc784df347696437b1e30e63156970f334
Farhad Rezazadeh <47423984+frezazadeh@users.noreply.github.com>
2024-05-22 15:07:27 +0200

    Update Coordinate_Ascent_for_Mask_Parameter.py

--- TANGO/Coordinate_Ascent_for_Mask_Parameter.py
+++ TANGO/Coordinate_Ascent_for_Mask_Parameter.py
@@ -1,1 +1,24 @@
+# Update each parameter using coordinate ascent
+for name, param in zip(['edge_mask', 'node_feat_mask'],
+                       [(edge_mask_loc, edge_mask_scale_positive),
+                        (node_feat_mask_loc, node_feat_mask_scale_positive)]):
 
+    # Sample other masks while keeping current mask fixed
+    if name == 'edge_mask':
+        node_feat_mask = Normal(node_feat_mask_loc, node_feat_mask_scale_positive).rsample()
+        x_masked = x * node_feat_mask
+    elif name == 'node_feat_mask':
+        edge_mask = Normal(edge_mask_loc, edge_mask_scale_positive).rsample()
+        edge_index_masked = edge_index[:, edge_mask > 0.5]
+        x_masked = x * node_feat_mask_loc
+
+    # Model execution to compute likelihood
+    out, node_output = self.model(x_masked, edge_index_masked, return_node_output=True)
+    nll = -node_output[node].sum()
+
+    # Coordinate update step
+    loc, scale = param
+    kl = Normal(loc, scale).log_prob(loc) - prior.log_prob(loc).sum()
+    elbo = nll + kl
+    elbo.backward()
+    optimizer.step()
